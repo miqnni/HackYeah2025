84e158992def863afeade44f201a2c5f431819f8
Horacjusz <pawel.prus05@gmail.com>
2025-10-04 22:05:00 +0200

    Current state

--- game/scripts/bolt.gd
+++ game/scripts/bolt.gd
@@ -1,16 +1,49 @@
 extends Node2D
 
-@onready var button: Button = $Button
+#@onready var button: Button = $Button
 @onready var rigid_body_2d: RigidBody2D = $RigidBody2D
 @onready var collision_shape_2d: CollisionShape2D = $RigidBody2D/CollisionShape2D
+@onready var hammer: Node2D = $"../../../Hammer"
+@onready var button: TextureButton =  $RigidBody2D/TextureButton
 
+var bolt_out = 0
+var bolt_threshold = 0.5
+var bolt_released = false
+
+var bolt_prob = 0.01
+var rng = RandomNumberGenerator.new()
+
+func _process(delta: float) :
+	if bolt_released :
+		return
+	var rand = rng.randf_range(0, 1)
+	if rand < bolt_prob :
+		bolt_out += 0.01
+	
+	button.modulate = Color(1, 0, 0, bolt_out)
+	
+	if bolt_out >= 1 :
+		release_bolt()
+	
 func _ready() :
 	rigid_body_2d.freeze = true
 
 func release_bolt() :
 	rigid_body_2d.freeze = false
-	rigid_body_2d.visible = false
+	#rigid_body_2d.visible = false
+	bolt_released = true
 
-func _on_button_button_down() -> void:
-	release_bolt()
-	button.visible = false
+func _on_texture_button_button_down() -> void:
+	if hammer.hammer_picked :
+		print(hammer.hammer_val)
+		if abs(hammer.hammer_val) < hammer.hammer_good_threshold :
+			bolt_out = 0
+		else :
+			if bolt_out > bolt_threshold :
+				bolt_out = 1
+			else :
+				bolt_out = 0
+		hammer.hammer_picked = false
+		return
+	
+	bolt_out = 1
--- game/scripts/hammer.gd
+++ game/scripts/hammer.gd
@@ -1,22 +1,33 @@
 extends Node2D
 
 @onready var texture_button: TextureButton = $TextureButton
+@onready var bar_dot: Sprite2D = $"../colorbar/bar_dot"
 
 var hammer_picked = false
 
 var hammer_val = 0
-var hammer_max = 100
-var hammer_min = -100
-var hammer_jump = 1
+var hammer_threshold = 145
+var hammer_jump = 2
+var hammer_good_threshold = 30
+
+@onready var dot_x_center = bar_dot.position.x
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		print("Wow, a left mouse click")
+	
 
 func _process(delta: float) :
 	if hammer_picked :
 		texture_button.disabled = true
-		if (hammer_val <= hammer_min and hammer_jump < 0) or (hammer_val >= hammer_max and hammer_jump > 0) :
-			hammer_jump *= -1.1
+		if (hammer_val <= -hammer_threshold and hammer_jump < 0) or (hammer_val >= hammer_threshold and hammer_jump > 0) :
+			hammer_jump *= -1.01
 		hammer_val += hammer_jump
-		hammer_val = min(max(hammer_val, hammer_min), hammer_max)
-	print(hammer_val)
+		hammer_val = min(max(hammer_val, -hammer_threshold), hammer_threshold)
+	else :
+		hammer_val = 0
+		texture_button.disabled = false
+	bar_dot.position.x = dot_x_center + hammer_val
 	
 func _on_texture_button_button_down() :
 	hammer_picked = true
