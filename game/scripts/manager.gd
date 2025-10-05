50a378bb6966d07e8644da8cdba61b86fa0eec64
miqnni <miqnni@protonmail.com>
2025-10-05 06:29:59 +0200

    animation with bugs when repeatedly switching or clicking

--- game/scripts/manager.gd
+++ game/scripts/manager.gd
@@ -9,7 +9,13 @@ var h_green=false
 var h_blue=false
 var h_del=false
 
-func _process(delta: float) -> void:
+func _ready() -> void:
+	$mixtures/green_kula.hide();
+	$mixtures/blue_kula.hide();
+	$mixtures/red_kula.hide();
+	$mixtures/beczka_kula.hide();
+
+func _process(_delta: float) -> void:
 	if h_red:
 		if Green>0:
 			Green-=1
@@ -34,32 +40,70 @@ func _process(delta: float) -> void:
 			Red+=1
 	#print("Red: ",Red," Green: ",Green," Blue: ",Blue)
 
+#func wait_before_next_animation_iteration(full_animation_time: float) -> bool:
+	#var remaining_time = (full_animation_time - $AnimationPlayer.current_animation_position) * (1 / $AnimationPlayer.speed_scale)
+	#print("> " + str(remaining_time))
+	#print("> " + str(remaining_time > 0.3 && remaining_time < full_animation_time * (1 / $AnimationPlayer.speed_scale)))
+	#return remaining_time > 0.3 * (1 / $AnimationPlayer.speed_scale) && remaining_time < full_animation_time * (1 / $AnimationPlayer.speed_scale)
+
+func wait_for_animation_finish(full_animation_time: float) -> void:
+	var remaining_time = (full_animation_time - $AnimationPlayer.current_animation_position) * (1 / $AnimationPlayer.speed_scale)
+	if remaining_time < 0:
+		remaining_time = 0
+	print(remaining_time)
+	await get_tree().create_timer(remaining_time * 0.9).timeout
+	pass
+
+# RED, 2.75
 func _on_button_button_down() -> void:
+	#if $AnimationPlayer.is_playing():
+		#return
+	$mixtures/red_kula.show()
+	$AnimationPlayer.play("red_kula")
 	h_red=true
 
 
 func _on_button_button_up() -> void:
+	await wait_for_animation_finish(2.75)
+	$AnimationPlayer.stop()
+	$mixtures/red_kula.hide()
 	h_red=false
 	
-
+# GREEN, 2.75
 func _on_button_2_button_down() -> void:
+	$mixtures/green_kula.show()
+	$AnimationPlayer.play("green_kula")
 	h_green=true
 	
 
 func _on_button_2_button_up() -> void:
+	await wait_for_animation_finish(2.75)
+	$AnimationPlayer.stop()
+	$mixtures/green_kula.hide()
 	h_green=false
 	
 
+# BLUE, 2.0
 func _on_button_3_button_down() -> void:
+	$mixtures/blue_kula.show()
+	$AnimationPlayer.play("blue_kula")
 	h_blue=true
 	
 
 func _on_button_3_button_up() -> void:
+	await wait_for_animation_finish(2.0)
+	$AnimationPlayer.stop()
+	$mixtures/blue_kula.hide()
 	h_blue=false
 	
-	
+# BECZKA, 2.0
 func _on_button_4_button_down() -> void:
+	$mixtures/beczka_kula.show()
+	$AnimationPlayer.play("beczka_kula")
 	h_del=true
 
 func _on_button_4_button_up() -> void:
+	await wait_for_animation_finish(2.0)
+	$AnimationPlayer.stop()
+	$mixtures/beczka_kula.hide()
 	h_del=false
