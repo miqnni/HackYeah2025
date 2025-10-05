c9009340ef537d07f0a96154a9bd4a074e60e416
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-10-05 08:15:01 +0200

    potki

--- game/scripts/client.gd
+++ game/scripts/client.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 @onready var main_area: Node2D = $".."
 
 var tex_table=[]
+var pot_table=[]
+var pot_fill_table=[]
 var order = [0,0,0]
 var score = 0
 
@@ -13,6 +15,18 @@ func _ready() -> void:
 	var tex5 = load("res://assets/npc5.png")
 	var tex6 = load("res://assets/npc6.png")
 	tex_table=[tex1,tex2,tex3,tex4,tex5,tex6]
+	var pot1 = load("res://assets/Potek 1 .png")
+	var pot1_f = load("res://assets/Potek 1 płyn .png")
+	var pot2 = load("res://assets/Potek 2.png")
+	var pot2_f = load("res://assets/Potek 2 płyn .png")
+	var pot3 = load("res://assets/Potek 3 .png")
+	var pot3_f = load("res://assets/Potek 3 płyn .png")
+	var pot4 = load("res://assets/Potek 4 .png")
+	var pot4_f = load("res://assets/Potek 4 płyn .png")
+	var pot5 = load("res://assets/Potek 5.png")
+	var pot5_f = load("res://assets/Potek 5 płyn .png")
+	pot_table=[pot1,pot2,pot3,pot4,pot5]
+	pot_fill_table=[pot1_f,pot2_f,pot3_f,pot4_f,pot5_f]
 
 func new_order():
 	order = [randi_range(0, 255),randi_range(0, 255),randi_range(0, 255)]
@@ -27,3 +41,8 @@ func submit():
 		#var nr = int(str(name)[6])
 		#print(nr)
 		main_area.free_client(0)
+
+func roll_pot_texture():
+	var tex = randi_range(0,4)
+	$ClientOrderPopUp/Sprite2D2.texture=pot_table[tex]
+	$ClientOrderPopUp/Sprite2D.texture=pot_fill_table[tex]
