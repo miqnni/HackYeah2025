f456a0deedc895c114fc21f85601d7ed40be1b53
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-10-05 10:34:09 +0200

    przyciski

--- game/scripts/menu.gd
+++ game/scripts/menu.gd
@@ -3,3 +3,16 @@ extends Node2D
 func _on_start_button_down() -> void:
 	hide()
 	$Comic.start()
+	
+
+func _on_options_button_down() -> void:
+	$"3places".hide()
+	$"2places".show()
+
+func _on_exit_button_down() -> void:
+	pass
+	
+	
+func _on_szczalka_button_down() -> void:
+	$"2places".hide()
+	$"3places".show()
